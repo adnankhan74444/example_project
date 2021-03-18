270d9638ffb868e2799d131c9aaa4a1add0d9ad4
Adnan khan <Akhan@sunbonn.com>
2021-03-18 18:20:36 +0500

    Cover bloc builder

--- lib/bloc/basic_explanation.dart
+++ lib/bloc/basic_explanation.dart
@@ -16,21 +16,26 @@
 // Stream of events
 
 // When should i use bloc as opposed to bloc?
-  // is related how you receive input from ui
+// is related how you receive input from ui
 // What is the big difference between bloc.cubit and bloc?
-
 
 // Major Concept
 //1. BlocBuilder
+  //The builder function can be called multiple times per state (due to flutter engine).
+  //A widget that helps RE-Building the UI based on bloc State changes
+  //BlocBuilder is a widget which aquire bloc or cubit and a builder function.
+  //The Builder function must be a pure function.
+
+// What is Pure function?
+// A pure function is only depands on function arguments is called pure function
+
+
 //2. BlocProvider
 // BlocProvider is a flutter widget which create and provide a bloc to all of his children.
 // BlocProvider is also known as dependency injection widget.so the single instance of a Bloc
 // can be provided to a multiple widget with a Subtree.
 
-//3. BlocListner
+//3. BlocListener
 
 // What exactly is a widget?
 // Widget is flutter are actually class which has own attributes and behaviour.
-
-
-
