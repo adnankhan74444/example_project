c61e4ff28e71176027943e692d4ff2a3ae11755d
Adnan khan <Akhan@sunbonn.com>
2021-03-19 17:55:35 +0500

    cover some topic.

--- lib/bloc/basic_explanation.dart
+++ lib/bloc/basic_explanation.dart
@@ -21,21 +21,59 @@
 
 // Major Concept
 //1. BlocBuilder
-  //The builder function can be called multiple times per state (due to flutter engine).
-  //A widget that helps RE-Building the UI based on bloc State changes
-  //BlocBuilder is a widget which aquire bloc or cubit and a builder function.
-  //The Builder function must be a pure function.
+//Re-builds the UI for every new state coming from the bloc.
+//The builder function can be called multiple times per state (due to flutter engine).
+//A widget that helps RE-Building the UI based on bloc State changes
+//BlocBuilder is a widget which acquire bloc or cubit and a builder function.
+//The Builder function must be a pure function.
+
+//aik masla ya hai es mai ky ya multiple times call houta hai jis mai agr
+//For example hmye navigate krwana ho user ko ya phr toast dekhna ho tou
+//wo bi multiple times call hoga.
 
 // What is Pure function?
-// A pure function is only depands on function arguments is called pure function
-
+// A pure function is only depends on function arguments is called pure function
 
 //2. BlocProvider
+//Creates and provide the only instance of a bloc to the subtree
 // BlocProvider is a flutter widget which create and provide a bloc to all of his children.
 // BlocProvider is also known as dependency injection widget.so the single instance of a Bloc
 // can be provided to a multiple widget with a Subtree.
 
 //3. BlocListener
+//The listner function is called only ones per state
+//(Not include the intial state)
+
+//4. BlocConsumer
+// Combines both bloc provider and bloc listener into a single widget
 
 // What exactly is a widget?
-// Widget is flutter are actually class which has own attributes and behaviour.
+// Widget is flutter are actually class which has own attributes and behavior
+
+//Bloc Architecture:
+// break into three layers:
+//1.UI Representation Layer
+//2.Business Logic Layer
+//3.Data layer
+
+//Data layer
+//divided into three layers
+//1.The repositories
+//2.The data providers
+//3.The Models
+
+//What is json?
+//Json is a collection of paired data
+
+//Local Access
+//providing an instance of bloc or cubit to a single screen is
+// known as local access
+
+//Route Access
+//providing an instance of bloc or cubit to multiple screen's is
+// known as local access
+
+//Flutter Routing Options
+//1. Anonymous Routing
+//2. Named Routing
+//3. Generated Routing
--- lib/main.dart
+++ lib/main.dart
@@ -115,38 +115,37 @@
 //     );
 //   }
 // }
-import 'package:example_project/screens/flexible_vs_expanded.dart';
-import 'package:example_project/screens/set_state.dart';
-import 'package:example_project/screens/sliver_appbar.dart';
-import 'package:example_project/screens/stateful_widget_lifecycle.dart';
-import 'package:example_project/screens/value_notif.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(new MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: new MyApp(),
-));
-
-
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SetStateVSValue',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: FlexibleVsExpanded(),
-      // home: CustomSliverHeaderDemo(),
-      // home: SetStateExample(),
-      // home: ValueNotifierWidget(),
-
-    );
-  }
-}
+// import 'package:example_project/screens/flexible_vs_expanded.dart';
+// import 'package:example_project/screens/set_state.dart';
+// import 'package:example_project/screens/sliver_appbar.dart';
+// import 'package:example_project/screens/stateful_widget_lifecycle.dart';
+// import 'package:example_project/screens/value_notif.dart';
+// import 'package:flutter/material.dart';
+// void main() => runApp(new MaterialApp(
+//   debugShowCheckedModeBanner: false,
+//   home: new MyApp(),
+// ));
+//
+//
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'SetStateVSValue',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: FlexibleVsExpanded(),
+//       // home: CustomSliverHeaderDemo(),
+//       // home: SetStateExample(),
+//       // home: ValueNotifierWidget(),
+//
+//     );
+//   }
+// }
 
 // class MainApp extends StatelessWidget {
 //   @override
@@ -175,4 +174,126 @@ class MyApp extends StatelessWidget {
 //   }
 // }
 
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/cubit/counter_cubit.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(),
+        child: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Incremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                } else if (state.wasIncremented == false) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Decremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'BRR, NEGATIVE ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'YAAAY ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue == 5) {
+                  return Text(
+                    'HMM, NUMBER 5',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else
+                  return Text(
+                    state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+              },
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  heroTag: Text('${widget.title}'),
+                  onPressed: () {
+                    context.read<CounterCubit>().decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  heroTag: Text('${widget.title} #2'),
+                  onPressed: () {
+                    // BlocProvider.of<CounterCubit>(context).increment();
+                    context.read<CounterCubit>().increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
